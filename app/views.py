0fd6af1d89d8795beb4f148b2322ad72f81e09fd
amanbaid99 <amanbaid99@gmail.com>
2020-08-15 22:50:05 +0530

    footer an dimag eadded

--- app/views.py
+++ app/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return render(request,"app/main.html") 
+def HomePage(request):
+    return render(request,"app/homepage.html") 
